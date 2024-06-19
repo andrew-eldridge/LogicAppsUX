3c54a8d0406e02e16ce1db12fe246c8a665d3d96
danielle <dacogbur@microsoft.com>
2024-06-19 10:10:29 -0700

    working version with v9 drawer

--- libs/data-mapper-v2/src/components/codeView/CodeView.tsx
+++ libs/data-mapper-v2/src/components/codeView/CodeView.tsx
@@ -1,119 +1,156 @@
-import { Stack } from '@fluentui/react';
-import { Button, Text } from '@fluentui/react-components';
-import { Code20Regular, Dismiss20Regular } from '@fluentui/react-icons';
-import type { MonacoProps } from '@microsoft/designer-ui';
-import { MonacoEditor } from '@microsoft/designer-ui';
-import { EditorLanguage } from '@microsoft/logic-apps-shared';
-import { useIntl } from 'react-intl';
-import { useSelector } from 'react-redux';
-import type { RootState } from '../../core/state/Store';
-import { useStyles } from './styles';
+import { Stack } from "@fluentui/react";
+import { Button, DrawerBody, DrawerHeader, DrawerHeaderTitle, InlineDrawer, Text } from "@fluentui/react-components";
+import { Code20Regular, Dismiss20Regular } from "@fluentui/react-icons";
+import type { MonacoProps } from "@microsoft/designer-ui";
+import { MonacoEditor } from "@microsoft/designer-ui";
+import { EditorLanguage } from "@microsoft/logic-apps-shared";
+import { useIntl } from "react-intl";
+import { useSelector } from "react-redux";
+import type { RootState } from "../../core/state/Store";
+import { useStyles } from "./styles";
 
 export const minCodeViewWidth = 320;
 
 export const commonCodeEditorProps: Partial<MonacoProps> = {
-  lineNumbers: 'on',
-  scrollbar: { horizontal: 'hidden', vertical: 'auto' },
-  height: '650px',
-  wordWrap: 'on',
-  wrappingIndent: 'same',
+  lineNumbers: "on",
+  scrollbar: { horizontal: "hidden", vertical: "auto" },
+  height: "650px",
+  wordWrap: "on",
+  wrappingIndent: "same",
 };
 
 export const CodeView = () => {
   const intl = useIntl();
   const styles = useStyles();
 
-  const dataMapDefinition = useSelector((state: RootState) => state.dataMap.present.curDataMapOperation.dataMapLML);
-  const isCodeViewOpen = useSelector((state: RootState) => state.panel.isCodeViewOpen);
-
-  const onStartDrag = (e: React.DragEvent) => {
-    // Show empty image in place of dragging ghost preview
-    const img = new Image();
-    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
-    e.dataTransfer.setDragImage(img, 0, 0);
-  };
+  const dataMapDefinition = useSelector(
+    (state: RootState) => state.dataMap.present.curDataMapOperation.dataMapLML
+  );
+  const isCodeViewOpen = useSelector(
+    (state: RootState) => state.panel.isCodeViewOpen
+  );
 
   const codeViewLoc = intl.formatMessage({
-    defaultMessage: 'Code view',
-    id: 'M0xrm+',
-    description: 'Code view title',
+    defaultMessage: "Code",
+    id: "M0xrm+",
+    description: "Code view title",
   });
 
   const closeCodeViewLoc = intl.formatMessage({
-    defaultMessage: 'Close code view',
-    id: '3sJlV+',
-    description: 'Close code view button',
+    defaultMessage: "Close code view",
+    id: "3sJlV+",
+    description: "Close code view button",
   });
 
   const noMapDefLoc = intl.formatMessage({
-    defaultMessage: 'Unable to generate data map definition',
-    id: 'sv+IcU',
+    defaultMessage: "Unable to generate data map definition",
+    id: "sv+IcU",
     description: `Message to display when the data map definition can't be generated`,
   });
 
-  return (
-    <Stack
-      style={{
-        display: isCodeViewOpen ? 'flex' : 'none',
-        width: minCodeViewWidth,
-        minWidth: '25%',
-      }}
-      horizontal={true}
-      verticalFill
-    >
-      <div
-        style={{
-          width: '10px',
-          cursor: isCodeViewOpen ? 'col-resize' : 'auto',
-        }}
-        onDragStart={onStartDrag}
-        draggable={isCodeViewOpen ? 'true' : 'false'}
-      />
-      <div className={styles.containerStyle}>
-        <Stack horizontal={false} verticalAlign="center" horizontalAlign="center">
-          <Stack
-            horizontal
-            verticalAlign="center"
-            style={{
-              width: '100%',
-              justifyContent: 'space-between',
-              marginBottom: '12px',
-              marginTop: '4px',
-            }}
-          >
-            <Stack horizontal verticalAlign="center">
-              <Code20Regular />
-              <Text className={styles.titleTextStyle} style={{ marginLeft: '8px' }}>
+  // return (
+  //   <div
+  //     className={mergeClasses(
+  //       styles.root,
+  //       isResizing && styles.rootResizerActive
+  //     )}
+  //   >
+  //     <div className={styles.container}>
+  //       <InlineDrawer
+  //         open
+  //         ref={sidebarRef}
+  //         className={styles.drawer}
+  //         style={{ width: `${sidebarWidth}px` }}
+  //         onMouseDown={(e) => e.preventDefault()}
+  //       >
+  //         <DrawerHeader>
+  //           <DrawerHeaderTitle>Code</DrawerHeaderTitle>
+  //         </DrawerHeader>
+  //         <DrawerBody>
+  //           <p>Resizable content</p>
+  //         </DrawerBody>
+  //       </InlineDrawer>
+  //       <ResizeComponent />
+  //     </div>
+  //     <p className={styles.content}>Resize the drawer to see the change</p>
+  //   </div>
+  // );
+
+  return(
+    <InlineDrawer open={isCodeViewOpen} className={styles.drawer}>
+      <DrawerHeader style={{display: 'flex', flexDirection: 'row'}}>
+        <Code20Regular />
+          <Text className={styles.titleTextStyle}>
                 {codeViewLoc}
               </Text>
-            </Stack>
+      </DrawerHeader>
+      <DrawerBody className={styles.drawerBody}>
+        <div className={styles.editorDiv}>
+          <MonacoEditor
+            language={EditorLanguage.yaml}
+            value={dataMapDefinition === '' ? noMapDefLoc : dataMapDefinition}
+            className={styles.editorStyle}
+            {...commonCodeEditorProps}
+            readOnly
+          />
+        </div>
+      </DrawerBody>
+    </InlineDrawer>
+  )
 
-            <Button
-              icon={<Dismiss20Regular />}
-              appearance="subtle"
-              //onClick={() => setIsCodeViewOpen(false)}
-              aria-label={closeCodeViewLoc}
-            />
-          </Stack>
+  // return (
+  //   <Stack
+  //     // style={{
+  //     //   display: isCodeViewOpen ? 'flex' : 'none',
+  //     //   width: minCodeViewWidth,
+  //     // }}
+  //     horizontal={true}
+  //     className={styles.stack}
+  //     verticalFill
+  //   >
+  //     <InlineDrawer
+  //       style={{
+  //         width: '10px',
+  //         cursor: isCodeViewOpen ? 'col-resize' : 'auto',
+  //       }}
+  //       draggable={isCodeViewOpen ? 'true' : 'false'}>
+  //     <div className={styles.containerStyle}>
+  //       <Stack horizontal={false} verticalAlign="center" horizontalAlign="center">
+  //         <DrawerHeader>
+  //           horizontal
+  //           verticalAlign="center"
+  //           {/* style={{
+  //             width: '100%',
+  //             justifyContent: 'space-between',
+  //             marginBottom: '12px',
+  //             marginTop: '4px',
+  //           }} */}
 
-          <div
-            style={{
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              flex: '1 1 auto',
-            }}
-          >
-            <MonacoEditor
-              language={EditorLanguage.yaml}
-              value={dataMapDefinition === '' ? noMapDefLoc : dataMapDefinition}
-              className={styles.editorStyle}
-              {...commonCodeEditorProps}
-              readOnly
-            />
-          </div>
-        </Stack>
-      </div>
-    </Stack>
-  );
+  //           <Stack horizontal verticalAlign="center">
+
+  //           </Stack>
+  //           </DrawerHeader>
+  //           <Button
+  //             icon={<Dismiss20Regular />}
+  //             appearance="subtle"
+  //             //onClick={() => setIsCodeViewOpen(false)}
+  //             aria-label={closeCodeViewLoc}
+  //           />
+  //         </Stack>
+
+  //         <div
+  //           className={styles.editorDiv}
+  //         >
+  //           <MonacoEditor
+  //             language={EditorLanguage.yaml}
+  //             value={dataMapDefinition === '' ? noMapDefLoc : dataMapDefinition}
+  //             className={styles.editorStyle}
+  //             {...commonCodeEditorProps}
+  //             readOnly
+  //           />
+  //         </div>
+  //         </div>
+  //     </InlineDrawer>
+  //   </Stack>
+  // );
 };
--- libs/data-mapper-v2/src/components/codeView/styles.ts
+++ libs/data-mapper-v2/src/components/codeView/styles.ts
@@ -1,21 +1,42 @@
-import { makeStyles, shorthands, tokens, typographyStyles } from '@fluentui/react-components';
+import {
+  makeStyles,
+  shorthands,
+  tokens,
+  typographyStyles,
+} from "@fluentui/react-components";
 
 export const useStyles = makeStyles({
+  drawer: {
+    minWidth: "25%",
+    ...shorthands.borderLeft("1px", "solid", "#e0e0e0"),
+    backgroundColor: "#E8F2FD",
+  },
   containerStyle: {
-    height: '100%',
-    width: '100%',
-    ...shorthands.overflow('hidden'),
-    ...shorthands.padding('12px'),
-    boxSizing: 'border-box',
-    backgroundColor: tokens.colorNeutralBackground1,
+    height: "100%",
+    width: "100%",
+    ...shorthands.overflow("hidden"),
+    ...shorthands.padding("12px"),
+    boxSizing: "border-box",
     ...shorthands.borderRadius(tokens.borderRadiusMedium),
   },
+  drawerBody: {
+    paddingTop: "0px",
+    marginTop: "0px",
+    backgroundColor: "inherit",
+  },
   titleTextStyle: {
+     marginLeft: '0px',
     ...typographyStyles.body1Strong,
   },
   editorStyle: {
-    ...shorthands.border('1px', 'solid', tokens.colorNeutralStroke1),
+    ...shorthands.border("1px", "solid", tokens.colorNeutralStroke1),
     ...shorthands.borderRadius(tokens.borderRadiusMedium),
-    ...shorthands.padding('10px'),
+    paddingTop: '10px',
+    backgroundColor: tokens.colorNeutralBackground1,
+  },
+  editorDiv: {
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
 });
